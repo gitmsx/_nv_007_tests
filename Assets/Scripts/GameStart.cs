389985f90263fa0dd6bd911a37e4a5feb0b90a7a
gitmsx <gitmsx@mail.ru>
2022-11-22 14:38:30 +0300

    Saved on 22.11.2022-14:38:29,81

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    private List<GameObject> listPoints = new List<GameObject>();
+    public GameObject PF_chess1;
+    public GameObject PF_chess2;
+
+    public GameObject PlaceToBox;
+
+    private float TimeToCheckCircle = 2;
+    private float TimeToCheck;
+    float cellSize = _global.Global_Scale;
+
+
+
+
+    void Start()
+    {
+
+
+
+        float scale_pf = _global.Global_Scale;
+        GameObject[] ChessTmp = new GameObject[2];
+
+        ChessTmp[0] = PF_chess1;
+        ChessTmp[1] = PF_chess2;
+
+        for (int i = 0; i < 20; i++)
+            for (int j = 0; j < 20; j++)
+            {
+                Vector3 NewPos = new Vector3(scale_pf * i, 0.1f, scale_pf * j);
+                var BoxClone = Instantiate(ChessTmp[(i + j) % 2], NewPos, Quaternion.identity);
+              
+            }
+
+    
+
+
+    
+
+
+        for (int i = 0; i < 2; i++)
+        {
+            Vector3 SpownPoint = new Vector3(10, 1, 10 + i);
+            var Place = Instantiate(PlaceToBox, SpownPoint, Quaternion.identity);
+            
+            listPoints.Add(Place);
+        }    
+        
+        
+        
+
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeToCheck += Time.deltaTime;
+        Debug.DrawRay(transform.position, -Vector3.up * cellSize, Color.green);
+
+
+        if (TimeToCheckCircle < TimeToCheck)
+        {
+            TimeToCheck = 0;
+           bool  status = CheckWin();
+        }
+
+
+    }
+
+
+    bool CheckWin()
+    {
+        int all_pnts = 0;
+        int all_Cheked = 0;
+        foreach (var PTs in listPoints)
+        {
+            all_pnts++;
+            if (PTs.GetComponent("PlaceBoxCheck"))
+            {
+
+            }
+        }
+        return false;
+    }
+
+
+
+
+
+}
--- Assets/Scripts/PlaceBoxCheck.cs
+++ Assets/Scripts/PlaceBoxCheck.cs
@@ -6,30 +6,21 @@ using UnityEngine;
 public class PlaceBoxCheck : MonoBehaviour
 {
 
+
+   public  bool Ffull = false;
+    float cellSize = _global.Global_Scale;
     private float TimeToCheckCircle = 2;
     private float TimeToCheck;
-    float cellSize = _global.Global_Scale;
-    LayerMask LayerBox = 8;
 
-    void Start()
-    {
-        TimeToCheck = 0;
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        
-        
         TimeToCheck += Time.deltaTime;
-        Debug.DrawRay(transform.position, -Vector3.up * cellSize , Color.green);
-        
-
+        Debug.DrawRay(transform.position, -Vector3.up * cellSize, Color.green);
         if (TimeToCheckCircle < TimeToCheck)
         {
             TimeToCheck = 0;
-           
-            if (PlaceBoxChek()) 
+            if (PlaceBoxChek())
             {
                 Debug.Log("Win!!!");
             }
@@ -42,23 +33,12 @@ public class PlaceBoxCheck : MonoBehaviour
 
     bool PlaceBoxChek()
     {
-
-        // Bit shift the index of the layer (8) to get a bit mask
         int layerMask = 1 << 8;
-
-        // This would cast rays only against colliders in layer 8.
-        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-        //  layerMask = ~layerMask;
-
         if (Physics.Raycast(transform.position, -Vector3.up * cellSize, out RaycastHit hit, cellSize, layerMask))
         {
-            Debug.Log(hit.point);
+            Ffull = true;
             return true;
         }
-
-        //Debug.Log("Not hited !!!");
-        //Debug.Log(transform.position);
-        
         return false;
     }
 }
