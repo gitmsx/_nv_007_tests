38994c233a3324b4e083b1fd5eacb78ddaa0430a
gitmsx <gitmsx@mail.ru>
2022-11-22 13:23:36 +0300

    Saved on 22.11.2022-13:23:35,57

--- Assets/Scripts/PlaceBoxCheck.cs
+++ Assets/Scripts/PlaceBoxCheck.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 public class PlaceBoxCheck : MonoBehaviour
 {
 
-    private float TimeToCheckCircle = 1;
+    private float TimeToCheckCircle = 2;
     private float TimeToCheck;
     float cellSize = _global.Global_Scale;
+    LayerMask LayerBox = 8;
 
     void Start()
     {
@@ -21,15 +22,16 @@ public class PlaceBoxCheck : MonoBehaviour
         
         
         TimeToCheck += Time.deltaTime;
-        Debug.DrawRay(transform.position, Vector3.up * cellSize*3, Color.green);
+        Debug.DrawRay(transform.position, -Vector3.up * cellSize , Color.green);
+        
 
         if (TimeToCheckCircle < TimeToCheck)
         {
             TimeToCheck = 0;
-            
+           
             if (PlaceBoxChek()) 
             {
-                
+                Debug.Log("Win!!!");
             }
         }
 
@@ -41,11 +43,22 @@ public class PlaceBoxCheck : MonoBehaviour
     bool PlaceBoxChek()
     {
 
-        if (Physics.Raycast(transform.position, Vector3.up * cellSize*3, out RaycastHit hit, cellSize))
-        { 
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        //  layerMask = ~layerMask;
+
+        if (Physics.Raycast(transform.position, -Vector3.up * cellSize, out RaycastHit hit, cellSize, layerMask))
+        {
             Debug.Log(hit.point);
-        return true;
+            return true;
         }
+
+        //Debug.Log("Not hited !!!");
+        //Debug.Log(transform.position);
+        
         return false;
     }
 }
