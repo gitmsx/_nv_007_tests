09bad6b8367e65dbaed515dba7ff5fe2abec333d
gitmsx <gitmsx@mail.ru>
2022-11-24 19:27:08 +0300

    Saved on 24.11.2022-19:27:08,01

--- Assets/Scripts/ReadMaps.cs
+++ Assets/Scripts/ReadMaps.cs
@@ -38,10 +38,7 @@ public class ReadMaps : MonoBehaviour
 
     public void Start1(int Level1)
     {
-        Debug.Log("public void Start1(int Level1)");
-        Debug.Log(Wall.ToString());
-        Debug.Log(Box.ToString());
-
+       
         List<string> parsed = ReadData(Level1);
         CreateLevel(parsed);
     }
--- Assets/Scripts/_main.cs
+++ Assets/Scripts/_main.cs
@@ -2,14 +2,21 @@
 
 using System;
 using System.Diagnostics;
+using UnityEngine.XR;
+using UnityEngine;
 
 public class _main 
 {
-
-
     public static void EntryPoint()
     {
+
+        GameObject _handl = GameObject.Find("_GameStart");
+        _handl.AddComponent<ReadMaps>();
+        _handl.GetComponent<ReadMaps>().Start1(3);
+
         ReadMaps readMaps = new ReadMaps();
+        
+
         readMaps.Start1(4);
         Console.WriteLine("Start //// ");
     }
